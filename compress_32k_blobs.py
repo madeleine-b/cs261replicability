d88cdb79cfa5812c2b819936641341c9f6e45d75
Madeleine Barowsky <mbarowsky@g.harvard.edu>
2021-03-24 22:06:57 -0400

    Add Python utility for randomly choosing 32kb blobs from the directory and computing the timing and compression ratio and outputting to a file

---
+++ compress_32k_blobs.py
@@ -0,0 +1,38 @@
+import os.path
+import os
+import glob
+import random
+import subprocess
+import numpy as np
+
+zlib_level = 1
+BYTES_IN_32KB = 1024 * 32
+SAMPLE_SIZE = 2
+
+all_files = [f for f in glob.iglob("data/" + '**/**', recursive=True) if os.stat(f).st_size > BYTES_IN_32KB]
+print("There are", len(all_files), "files larger than 32 KB")
+
+
+result_dict = {}
+COMPRESSION_RATIO_TOL = 0.01
+for i in np.arange(0.0, 1.1, COMPRESSION_RATIO_TOL):
+	result_dict[float(np.round(i, 2))] = []
+
+for i in range(SAMPLE_SIZE):
+	file_name = random.choice(all_files)
+	file_bytes = os.stat(file_name).st_size
+	random_start_offset = random.choice(range(file_bytes // BYTES_IN_32KB))
+	results = subprocess.run(["./zlib_utility", file_name, str(zlib_level), 
+		 					 str(random_start_offset), str(-1 + random_start_offset + BYTES_IN_32KB)], 
+						 	 check=True, capture_output=True).stdout.split()
+	us_duration = int(results[0])
+	ratio = float((np.round(float(results[2]), 2)))
+	result_dict[ratio].append(us_duration)
+
+result_file_name = "timing_results_level_%d_samples_%d.txt" % (zlib_level, SAMPLE_SIZE)
+with open(result_file_name, "w") as f:
+	for k, v in result_dict.items():
+		f.write(str(k))
+		f.write(" ")
+		f.write(str(v))
+		f.write("\n")
